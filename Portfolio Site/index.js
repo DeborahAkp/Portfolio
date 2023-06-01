04169c3013cccbd1e2e6fc712cedd587079200f1
Deborah Akpoguma <deborahakpoguma@gmail.com>
2023-05-31 23:26:40 -0400

    updated site and project 1

--- Portfolio Site/index.js
+++ Portfolio Site/index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public/images'));
+app.use(express.static('public/files'));
+
+app.use(express.urlencoded({ extended: false }));
 
 
 app.set('view engine', 'ejs')
@@ -9,9 +15,15 @@ app.get('/', (req, res) => {
     res.render('homePage')
 })
 
+
 app.get('/portfolio', (req, res) => {
     res.render('portfolio')
 })
+
+app.get('/portfolio/project-one', (req, res) => {
+    res.render('projectOne')
+})
+
 
 app.get('/contact', (req, res) => {
     res.render('contactMe')
@@ -20,3 +32,5 @@ app.get('/contact', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
+
+app.use(express.static('public'));
